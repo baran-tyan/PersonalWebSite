af417429676712805b840d0c2f2f74a189fc3a53
Mariia Bierdisheva <mary312@ukr.net>
2018-11-08 17:10:04 +0200

    Initial

---
+++ js/index.js
@@ -0,0 +1,30 @@
+function dynamicallyLoadScript(url) {
+    var script = document.createElement('script');
+    script.src = url;
+
+    document.head.appendChild(script);
+}
+
+var pathArr = window.location.pathname.split( '/' ),
+pageName = pathArr[pathArr.length - 1];
+
+switch(pageName) {
+  case '':
+  case 'index.html':
+    dynamicallyLoadScript('js/hotkeys.js');
+    dynamicallyLoadScript('js/gallery.js');
+    dynamicallyLoadScript('js/appearance.js');
+    break;
+  case 'blog.html':
+    dynamicallyLoadScript('js/validation.js');
+    dynamicallyLoadScript('js/appearance.js');
+    break;
+  case 'ai.html':
+  case 'blog.html':
+  case 'hobbies.html':
+  case 'plans.html':
+  case 'computer_access.html':
+  case 'programming_languages.html':
+  case 'subjects.html':
+    dynamicallyLoadScript('js/appearance.js');
+}
